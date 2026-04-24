5c4a6c8f5650280e66f7ac7cf7e2bbe34703ce3c
Yuto Gushiken <yuyuyu11301213@gmail.com>
2026-04-24 19:44:07 +0900

    Bump service worker cache

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@
 // =====================================================
 
 const CACHE_PREFIX = "gushiken-design-";
-const CACHE_STAMP = "v20260424191424";
+const CACHE_STAMP = "v20260424194406";
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_STAMP}`;
 
 const STATIC_ASSETS = [
