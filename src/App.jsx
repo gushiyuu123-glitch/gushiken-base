ecab80546d0dc7ef52405f443412d40a8ad0b116
Yuto Gushiken <yuyuyu11301213@gmail.com>
2026-06-14 12:49:48 +0900

    fix: match footer import casing

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
 import Seo from "./components/Seo";
 
 import NavGlobal from "./components/NavGlobal";
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 import LenisManager from "./components/Lenis";
 
 // Pages
